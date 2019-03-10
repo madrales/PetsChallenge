932f83e37fb4b6a01b4ae59ef8ad09b11fe498cb
Mason Adrales <masonadrales@mwc-106076.dhcp.missouri.edu>
2019-03-10 16:37:49 -0500

    added Cat Class

---
+++ Pets/Pets/Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Pets
+{
+    public class Cat : Pet
+    {
+        public Cat(string name, string owner, double weight) : base(String.Empty, name, owner, weight)
+        {
+            this.petName = name;
+            this.petOwner = owner;
+            this.petWeight = weight;
+        }
+        public string meow(int count)
+        {
+            string sound = "";
+            for (int i = 0; i < count; i++)
+            {
+                sound = sound + "meow";
+            }
+            return sound;
+        }
+    }
+}
